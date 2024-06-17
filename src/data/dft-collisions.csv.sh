c944d9d434aa6c2230695f335c6ce5efa8933f5f
Adam roberts <adamben.roberts@gmail.com>
2024-06-17 16:17:47 +0200

    loader dir change

--- src/data/dft-collisions.csv.sh
+++ src/data/dft-collisions.csv.sh
@@ -1,7 +1,7 @@
 # URL="https://data.dft.gov.uk/road-accidents-safety-data/dft-road-casualty-statistics-collision-1979-latest-published-year.csv"
 
 # Use the data loader cache directory to store the downloaded data.
-TMPDIR=".observablehq/cache"
+TMPDIR=".observablehq/cache/data"
 
 # # Download the data (if it’s not already in the cache).
 if [ ! -f "$TMPDIR/dft-collisions.csv" ]; then
